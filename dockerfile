030f62473e0d328f1dfcdb6acb37ee121528a998
guillheu <guillheu@gmail.com>
2023-08-25 20:30:21 +0200

    ouah ca cogne de fou

--- dockerfile
+++ dockerfile
@@ -1,6 +1,16 @@
-FROM python:3.10.12-alpine3.18
+# FROM node:20.5.1-alpine3.18 as build-frontend
 
-COPY frontend/* /www/html/
+# COPY frontend/* /app
+
+# WORKDIR /app
+
+# RUN npm run build
+
+# RUN ls
+
+
+
+FROM python:3.10.12-alpine3.18
 
 COPY server/requirements.txt /python/requirements.txt
 
@@ -13,5 +23,7 @@ WORKDIR /python/src
 COPY docker/docker-entrypoint.sh /docker-entrypoint.sh
 
 COPY docker/default_env /default_env
+
+COPY frontend/* /www/html/
 
 ENTRYPOINT /docker-entrypoint.sh
--- server/src/main.py
+++ server/src/main.py
@@ -31,8 +31,6 @@ def root():
 def ingresses():
     return FileResponse(f"{HTML_DIR}/ingresses.json")
 
-
-
 def watch_ingresses():
     try:
         # Load configuration inside the Pod
@@ -45,7 +43,22 @@ def watch_ingresses():
 
     w = watch.Watch()
     for event in w.stream(api_instance.list_ingress_for_all_namespaces):
-        print("Event: %s %s %s" % (event['type'],event['object'].kind, event['object'].metadata), flush=True)
+        print("Event: %s %s %s" % (event['type'],event['object'].kind, event['object'].metadata.name), flush=True)
+        refresh_ingresses()
+
+def watch_ingress_classes():
+    try:
+        # Load configuration inside the Pod
+        config.load_incluster_config()
+    except ConfigException:
+        # Load configuration for testing
+        config.load_kube_config()
+
+    api_instance = client.NetworkingV1Api()
+
+    w = watch.Watch()
+    for event in w.stream(api_instance.list_ingress_class):
+        print("Event: %s %s %s" % (event['type'],event['object'].kind, event['object'].metadata.name), flush=True)
         refresh_ingresses()
 
 def refresh_ingresses():
@@ -60,10 +73,29 @@ def refresh_ingresses():
     api_instance = client.NetworkingV1Api()
 
     # List all ingresses in all namespaces
-    ingresses = api_instance.list_ingress_for_all_namespaces()
+    ingress_classes = api_instance.list_ingress_class()
 
     # Extract relevant ingress data
-    ingresses_data = []
+    ingress_classes_data = []
+    default_ingress_class = None
+    for ingress_class in ingress_classes.items:
+        ingress_class_info = {
+            'ingress_class_name': ingress_class.metadata.name,
+            'controller': ingress_class.spec.controller,
+            'lb_ip': find_load_balancer_ip_by_ingress_class(ingress_class.metadata.name),
+            'ingresses': []
+        }
+        ingress_classes_data.append(ingress_class_info)
+        try:
+            if ingress_class.metadata.annotations["ingressclass.kubernetes.io/is-default-class"] == 'true':
+                default_ingress_class = ingress_class.metadata.name
+                print(f"{ingress_class.metadata.name} is the default ingress class.")
+        except:
+            pass
+
+    # List all ingresses in all namespaces
+    ingresses = api_instance.list_ingress_for_all_namespaces()
+
     for ingress in ingresses.items:
         ingress_info = {
             'name': ingress.metadata.name,
@@ -71,13 +103,13 @@ def refresh_ingresses():
             'rules': []
         }
         try:
-            ingress_info['cert-manager cluster issuer'] = ingress.metadata.annotations["cert-manager.io/cluster-issuer"]
+            ingress_info['cert_manager_cluster_issuer'] = ingress.metadata.annotations["cert-manager.io/cluster-issuer"]
         except:
             print(f"could not find \"cert-manager.io/cluster-issuer\" annotation for ingress {ingress.metadata.name}")
-        try:
-            ingress_info['ingress class name'] = ingress.spec.ingress_class_name
-        except:
-            print(f"could not find spec field ingressClassName for ingress {ingress.metadata.name}")
+        # try:
+        #     ingress_info['ingress_class_name'] = ingress.spec.ingress_class_name
+        # except:
+        #     print(f"could not find spec field ingressClassName for ingress {ingress.metadata.name}")
         for rule in ingress.spec.rules:
             ingress_rule = {
                 'host': rule.host,
@@ -97,15 +129,55 @@ def refresh_ingresses():
                             ruse["tls"] = 'false'
         except:
             print(f"TLS configuration not found for ingress {ingress.metadata.name}")
-        ingresses_data.append(ingress_info)
+        for ingress_class in ingress_classes_data:
+            if ingress_class["ingress_class_name"] == ingress.spec.ingress_class_name:
+                ingress_class["ingresses"].append(ingress_info)
+                
+    
     with open(f"{HTML_DIR}/ingresses.json", 'w') as f:
-        json.dump(ingresses_data, f)
+        json.dump(ingress_classes_data, f)
 
-    # Serve the updated ingresses.json file
-    return FileResponse(f"{HTML_DIR}/ingresses.json")
+from kubernetes import client, config
+
+def find_load_balancer_ip_by_ingress_class(ingress_class_name):
+    config.load_kube_config() # Or config.load_incluster_config()
+    networking_v1 = client.NetworkingV1Api()
+    apps_v1 = client.AppsV1Api()
+    v1 = client.CoreV1Api()
+
+    # Get Ingress Class
+    ingress_class = networking_v1.read_ingress_class(ingress_class_name)
+    controller_name = ingress_class.spec.controller.split("/")[-1]
+
+    # Search for the Deployment/StatefulSet/DaemonSet responsible for the controller (modify this as needed)
+    deployments = apps_v1.list_deployment_for_all_namespaces(label_selector=f"app.kubernetes.io/instance={controller_name}")
+
+    if not deployments.items:
+        print("No matching deployments found.", flush=True)
+        return
+    if len(deployments.items) > 1:
+        print("More than one deployment matching ; cannot find ingress controller pod.", flush=True)
+        return
+
+    # Extract the labels of the first matching deployment
+    deployment_labels = deployments.items[0].spec.selector.match_labels
+    namespace = deployments.items[0].metadata.namespace
+
+    # Find Services that select those labels in the same namespace as the deployment
+    services = v1.list_namespaced_service(namespace, label_selector=",".join([f"{k}={v}" for k, v in deployment_labels.items()]))
+    for service in services.items:
+        if service.spec.type == 'LoadBalancer':
+            return service.status.load_balancer.ingress[0].ip
+
+    print("No matching LoadBalancer service found.", flush=True)
+    return None
+
+
 
 # Function to run on startup
 @app.on_event("startup")
 async def startup_event():
     threading.Thread(target=watch_ingresses, daemon=True).start()
-    refresh_ingresses()
+    threading.Thread(target=watch_ingress_classes, daemon=True).start()
+    # refresh_ingresses()
+    # refr
